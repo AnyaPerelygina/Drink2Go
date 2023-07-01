43fb04c55210af6df4cad374744332cc90c3dc54
Anna <mynameisannanas@gmail.com>
2023-07-01 21:27:56 +0200

    feat: header

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+let navMain = document.querySelector('.nav');
+let navToggle = document.querySelector('.nav__toggle');
+
+navMain.classList.remove('nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('nav--closed')) {
+    navMain.classList.remove('nav--closed');
+    navMain.classList.add('nav--opened');
+  } else {
+    navMain.classList.add('nav--closed');
+    navMain.classList.remove('nav--opened');
+  }
+});
