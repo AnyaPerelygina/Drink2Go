1cafd2124db601ee29c72f061de625f1f2643ac4
Anna <mynameisannanas@gmail.com>
2024-02-22 14:13:30 +0100

    fix: скорректирована работа range и input

--- source/js/modules/filters/filters.js
+++ source/js/modules/filters/filters.js
@@ -2,34 +2,67 @@ import noUiSlider from 'nouislider';
 
 const initRangeSliders = () => {
   const rangeSliders = document.querySelectorAll('.range-slider');
+  const minInput = document.getElementById('minCost');
+  const maxInput = document.getElementById('maxCost');
 
   for (let i = 0; i < rangeSliders.length; i++) {
     if (rangeSliders[i].getAttribute('data-range-slider') === 'price') {
-      noUiSlider.create(rangeSliders[i], {
-        start: [0, 700],
+      const slider = noUiSlider.create(rangeSliders[i], {
+        start: [0, 900],
         connect: true,
         range: {
           min: 0,
-          max: 900,
+          max: 1100,
         },
         tooltips: [true, true],
         format: {
-          to: (value) => Math.round(value).toLocaleString('ru'),
-          from: (value) => Number(value),
+          to: (value) => Math.round(value),
+          from: (value) => Number(value.replace(/\D/g, '').replace(/\s/g, '')),
         },
       });
 
-      const handleUpper = rangeSliders[i].querySelector('.noUi-handle-upper');
-      const tooltip = handleUpper.querySelector('.noUi-tooltip');
+      slider.on('update', (values) => {
+        const [min, max] = values;
+        minInput.value = Math.round(min).toLocaleString('ru');
+        maxInput.value = Math.round(max).toLocaleString('ru');
+      });
 
-      rangeSliders[i].noUiSlider.on('update', () => {
-        const value = rangeSliders[i].noUiSlider.getPositions();
+      minInput.addEventListener('input', () => {
+        // eslint-disable-next-line radix
+        let minVal = parseInt(minInput.value.replace(/\D/g, ''));
+        // eslint-disable-next-line radix
+        let maxVal = parseInt(maxInput.value.replace(/\D/g, ''));
 
-        if (value[1] > 90) {
-          tooltip.style.left = '-15px';
-        } else {
-          tooltip.style.left = '0';
+        if (isNaN(minVal)) {
+          minVal = 0;
         }
+
+        if (minVal > maxVal) {
+          minVal = maxVal;
+        }
+
+        slider.set([minVal, maxVal]);
+      });
+
+      maxInput.addEventListener('input', () => {
+        // eslint-disable-next-line radix
+        let minVal = parseInt(minInput.value.replace(/\D/g, ''));
+        // eslint-disable-next-line radix
+        let maxVal = parseInt(maxInput.value.replace(/\D/g, ''));
+
+        if (isNaN(maxVal)) {
+          maxVal = 1100;
+        }
+
+        if (maxVal > 1100) {
+          maxVal = 1100;
+        }
+
+        if (maxVal < minVal) {
+          maxVal = minVal; // Если максимальное значение меньше минимального, делаем их равными
+        }
+
+        slider.set([minVal, maxVal]);
       });
     }
   }
